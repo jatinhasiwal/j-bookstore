19e8d6c3b36d66517b28118d3bbb6f404570a593
Jatin Hasiwal <140293091+jatinhasiwal@users.noreply.github.com>
2024-12-03 10:20:24 +0530

    user-model.js

--- models/user-model.js
+++ models/user-model.js
@@ -26,7 +26,8 @@ const UserSchema = new mongoose.Schema({
     
     avatar: {
         type: String,
-        default: "https://cdn-icons-png.flaticon.com/512/9187/9187604.png"
+        default: "https://img.freepik.com/free-photo/3d-portrait-businessman_23-2150793877.jpg?w=360&t=st=1729312894~exp=1729313494~hmac=6f75eb3226b826f8819c8a7da5d53f0e309c0ea8a9ad3593572a55a7f405c546
+"
     },
     role: {
         type: String,
